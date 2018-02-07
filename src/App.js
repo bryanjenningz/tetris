26b275c9bb49692b4c429bef8ebbcd92f490026b
Bryan Jennings <bryanjenningz@gmail.com>
2018-02-06 18:34:59 -0800

    basic tick logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
 import React, { Component } from "react";
 import "./App.css";
 
+// const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 const blocks = ["7", "L", "S", "Z", "O", "I"];
 const range = (lo, hi) => Array.from({ length: hi - lo + 1 }, (_, i) => lo + i);
 const randomInt = (lo, hi) => Math.floor(lo + Math.random() * (hi - lo + 1));
 const randomChoice = choices => choices[randomInt(0, choices.length - 1)];
 
-const BLOCK_WIDTH = 10;
+const scoreToDropTime = score => {
+  const minScoreToTime = {
+    0: 1000,
+    10: 900,
+    20: 800,
+    30: 700,
+    40: 600,
+    50: 500,
+    60: 450,
+    70: 400,
+    80: 350,
+    90: 300,
+    100: 250,
+    110: 225,
+    120: 200
+  };
+  let dropTime = null;
+  for (const minScore in minScoreToTime) {
+    if (score >= Number(minScore)) {
+      dropTime = minScoreToTime[minScore];
+    }
+  }
+  return dropTime;
+};
+
+const isCollision = ({ block, x, y, rotation, grid }) => {};
+
+const addBlockToGrid = ({ block, x, y, rotation, grid }) => {};
+
+const BLOCK_WIDTH = 50;
 const SCREEN_WIDTH = 500;
+const INITIAL_X = 4;
+const INITIAL_Y = 0;
 
 class App extends Component {
   state = {
     block: randomChoice(blocks),
     futureBlocks: range(0, 300).map(() => randomChoice(blocks)),
-    x: 0,
-    y: 0,
-    grid: range(0, 9).map(() => range(0, 9).map(() => null))
+    x: INITIAL_X,
+    y: INITIAL_Y,
+    rotation: 0,
+    grid: range(0, 9).map(() => range(0, 9).map(() => null)),
+    score: 0,
+    lastDropTime: Date.now()
   };
+
+  componentDidMount() {
+    const tick = () => {
+      const { lastDropTime, score, x, y, block, rotation, grid } = this.state;
+      const dropTime = scoreToDropTime(score);
+      if (lastDropTime + dropTime >= Date.now()) {
+        if (isCollision({ block, x, y: y + 1, rotation, grid })) {
+          const addedGrid = addBlockToGrid({ block, x, y, rotation, grid });
+          const fullRows = addedGrid.filter(row => row.every(color => color));
+          const fullRowsRemovedGrid = [
+            ...fullRows.map(() => range(0, 9).map(() => null)),
+            ...addedGrid.filter(row => row.some(color => !color))
+          ];
+          this.setState({
+            x: INITIAL_X,
+            y: INITIAL_Y,
+            score: score + fullRows.length,
+            lastDropTime: Date.now(),
+            grid: fullRowsRemovedGrid
+          });
+        } else {
+          this.setState({ y: y + 1, lastDropTime: Date.now() });
+        }
+      }
+    };
+    setInterval(tick, 10);
+  }
 
   render() {
     const { grid } = this.state;
@@ -26,7 +88,8 @@ class App extends Component {
           width: SCREEN_WIDTH,
           height: SCREEN_WIDTH,
           background: "white",
-          margin: "0 auto"
+          margin: "0 auto",
+          position: "relative"
         }}
       >
         {grid.map((row, y) => (
@@ -35,6 +98,9 @@ class App extends Component {
               <div
                 key={x}
                 style={{
+                  position: "absolute",
+                  left: x * BLOCK_WIDTH,
+                  top: y * BLOCK_WIDTH,
                   width: BLOCK_WIDTH,
                   height: BLOCK_WIDTH,
                   background: color
